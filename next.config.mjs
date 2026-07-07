d6780cb5f9297a370226678ad14ca98dccd962f4
Oshan Avishka <oshanavishka06@gmail.com>
2026-07-07 11:18:13 +0530

    feat : docker-config-added

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
+  output: 'standalone',
   allowedDevOrigins: [
     'ais-dev-4px4mwjktqru3ns3gi2sgh-361291877326.asia-southeast1.run.app',
     'ais-pre-4px4mwjktqru3ns3gi2sgh-361291877326.asia-southeast1.run.app',
